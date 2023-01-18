dd9f0b248e9a1b4ee6e6a4c875578162c5ce0327
rianmanhente <rian.valeente@gmail.com>
2023-01-18 19:11:16 -0300

    changes

--- sections/Informacoes.tsx
+++ sections/Informacoes.tsx
@@ -6,7 +6,7 @@ export default function titleComponent() {
   return (
     <>
       <div class="mt-2">
-        <p class="text-center font-xl text-black font-bold">The All-Business</p>
+        <p class="text-center font-xl text-black font-bold">Meu deus do ceu</p>
       </div>
       <div>
         <img
@@ -14,7 +14,7 @@ export default function titleComponent() {
           w-16 h-16 m-auto mt-16"
           src="/imagem_perfil.png"
           alt=""
-          />
+        />
       </div>
       <div>
         <h1>Como colocar funcoes aqui?</h1>
--- sections/testando.tsx
+++
@@ -1,7 +0,0 @@
-export interface Props {
-  title: string;
-}
-
-export default function ({ title }: Props) {
-  return <div class="text-center bg-black font-bold">{title}</div>;
-}
